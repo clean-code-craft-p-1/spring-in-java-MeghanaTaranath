98e868f84a3b91118daedf32fb445a5f3cc1efe3
MeghanaTaranath <mtaranath91@gmail.com>
2025-05-17 17:46:29 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Statistics 
 {
     public static Stats getStatistics(List<float> numbers) {
-        //implement the computation of statistics here
+        return 0.001f;        
     }
 }
